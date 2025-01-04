8b58867e3ae49d8ca2e8fb21300a1865b8f9243d
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2025-01-03 20:32:59 -0600

    Hero ship, Alien ship, Game Manager

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+class GameManager {
+    round(hero, armada) {
+        let turn = 0;
+        while(hero.hull > 0 || armada.length > 0) {
+            //turn
+
+            //if alien ship is destroyed after attack grant option to retreat and exit loop
+
+
+        }
+    }
+}
+
+
 class Ship {
     //hull, firepower, accuracy
+    constructor(hull, firepower, accuracy) {
+        this.hull = hull,
+        this.firepower = firepower, 
+        this.accuracy = accuracy
+    }
 }
+
+class AlienShip extends Ship{
+    constructor()
+}
+
+let heroShip = Ship(20, 5, .7);
