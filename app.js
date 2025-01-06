7ffb081e96a9eeb963e108567faca26fbb367f51
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2025-01-06 04:18:54 -0600

    prompt messages

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const startContainer = document.getElementById('start');
 const contentScreen = document.getElementById('display');
 const preSession = document.getElementById('preScreen');
 const session = document.getElementById('gameplay');
-const attackButton = document.ElementById('attack');
-const retreatButton = document.ElementById('retreat');
-const feedback = document.getElementById('topPanel');
+const attackButton = document.getElementById('attack');
+const retreatButton = document.getElementById('retreat');
+const feedback = document.getElementById('panel1');
+const lowerFeedback = document.getElementById('panel2');
+const playerOne = document.getElementById('player');
 
 
 const gameStart = () => {
@@ -23,24 +25,34 @@ const gameStart = () => {
 startButton.addEventListener('click', () => {
     gameStart();
 });
+
+
 class GameManager {
     constructor(hero, armada) {
         this.hero = hero;
         this.armada = armada;
         this.currentAlien = this.armada[0];
     }
+    win() {
+
+    }
+
     round() {
         if(this.currentAlien.hull <= 0) {
             console.log("Alien Ship Destroyed");
+            lowerFeedback.innerText = "Alien Ship Destroyed"
             this.armada.shift();
             if(this.armada.length === 0) {
                 console.log("All ships destroyed");
+                lowerFeedback.innerText = "All ships destroyed";
                 return;
             }
             this.currentAlien = this.armada[0];
         }
         if(this.hero.hull <= 0) {
             console.log('Your ship has been destroyed!');
+            playerOne.style.display = 'none';
+
         }
     }
 
@@ -55,26 +67,31 @@ class GameManager {
 
 class Ship {
     //hull, firepower, accuracy
-    constructor(hull, firepower, accuracy) {
+    constructor(hull, firepower, accuracy, type) {
         this.hull = hull,
         this.firepower = firepower, 
-        this.accuracy = accuracy
+        this.accuracy = accuracy,
+        this.type = type;
     }
 
     attack(target) {
         if(Math.random() < this.accuracy) { //hit
             console.log("target hit");
+            feedback.innerText = this.type + " hits " + target.type;
             target.hull -= this.firepower; 
 
             if(target.hull <= 0) {
                 target.hull = 0;
+                /*
                 console.log("target destroyed");
+                feedback.innerText = "Target Destroyed";
+                */
             }
         }
         else {
             console.log("miss");
+            feedback.innerText = this.type + "misses " + target.type;
         }
-
     }
 }
 
@@ -83,11 +100,11 @@ class AlienShip extends Ship{
 }
 
 
-const hero = new Ship(20, 5, 0.7);
+const hero = new Ship(20, 5, 0.7, "Player");
 const armada = [
-    new Ship(15, 4, 0.6),
-    new Ship(12, 3, 0.5),
-    new Ship(10, 2, 0.4),
+    new Ship(15, 4, 0.6, "Alien"),
+    new Ship(12, 3, 0.5, "Alien"),
+    new Ship(10, 2, 0.4, "Alien"),
 ];
 
 const gameManager = new GameManager(hero, armada);
@@ -99,7 +116,7 @@ attackButton.addEventListener('click', () => {
 
         
         if(gameManager.armada.length > 0) {
-            setTimeout(() => gameManager.alienTurn(), 1000); 
+            setTimeout(() => gameManager.alienTurn(), 1500); 
         }
     }
 })
